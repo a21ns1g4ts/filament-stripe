f6f851ea80425b0ecf64718329f20c96fa12d2dc
a21ns1g4ts <atila.danvi@outlook.com>
2025-02-20 04:58:12 -0300

    Change webhook route method to accept any HTTP verb

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use A21ns1g4ts\FilamentStripe\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('stripe/webhook', [WebhookController::class, 'handleWebhook'])->name('webhook');
+Route::any('stripe/webhook', [WebhookController::class, 'handleWebhook'])->name('webhook');
